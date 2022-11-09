834d69ee90c6016df21c1b0bf8c30336b754032d
GustiRafi <gustirafi49@gmail.com>
2022-11-09 14:02:16 +0700

    nyoba

--- resources/views/home/detail-wisata.blade.php
+++ resources/views/home/detail-wisata.blade.php
@@ -61,6 +61,9 @@
                                         <button class="nav-link active" id="pills-overview-tab" data-bs-toggle="pill"
                                             data-bs-target="#pills-overview" type="button" role="tab"
                                             aria-controls="pills-overview" aria-selected="true">aan</button>
+                                        <button class="nav-link active" id="pills-overview-tab" data-bs-toggle="pill"
+                                            data-bs-target="#pills-overview" type="button" role="tab"
+                                            aria-controls="pills-overview" aria-selected="true">aan</button>
                                     </li>
                                     <li class="nav-item" role="presentation">
                                         <button class="nav-link" id="pills-lokasi-tab" data-bs-toggle="pill"
